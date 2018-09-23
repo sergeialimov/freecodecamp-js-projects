6da00b0e2ed30199b9f9908b48690196522ddd80
Sergei Alimov <8912652@gmail.com>
2018-09-23 18:06:42 +0300

    move getMoneyFromDrawer from main function

--- cash-register.js
+++ cash-register.js
@@ -1,3 +1,12 @@
+// подсчет приготовленной сдачи
+const getBalance = (change) => {
+  if (change.length > 0) {
+    return change.map((x) => x[1])
+      .reduce((accumulator, currentValue) => accumulator + currentValue[1]);
+  }
+  return 0;
+};
+
 // @price - сумма к оплате
 // @banknote - банкнота
 const checkCashRegister = (price, banknote, cashInput) => {
@@ -10,7 +19,7 @@ const checkCashRegister = (price, banknote, cashInput) => {
     0.05: 'NICKEL',
     0.1: 'DIME',
     0.25: 'QUARTER',
-    1: 'DOLLAR',
+    1: 'ONE',
     5: 'FIVE',
     10: 'TEN',
     20: 'TWENTY',
@@ -19,9 +28,7 @@ const checkCashRegister = (price, banknote, cashInput) => {
 
   const result = { status: 'INSUFFICIENT_FUNDS', change: [] };
 
-  // функция складывает сдачу
-  // проверяет, есть ли уже такие номиналы. Если есть, то добавляет к существующему,
-  // если нет, то добавляет новый
+  // Если номинал существуе - прибавляет, если нет, то добавляет новый
   // @sum - сумма, которую необходимо добавить к сдаче
   // @amount - номинал банкноты
   const putChange = (amount, sum) => {
@@ -42,45 +49,43 @@ const checkCashRegister = (price, banknote, cashInput) => {
     console.log('result.change', result.change);
   };
 
-  // @change - общая сдача
-  // !!!!!!!!! сделать чтобы метод не падал с пустым массивом !!!!!!!!!!
-  const getBalance = (change) => change.map((x) => x[1])
-    .reduce((accumulator, currentValue) => accumulator + currentValue[1]);
+  // задача функции - уменьшить кол-во денег в кеше
+  // @amount - номинал
+  // @name - название банкноты
+  const getMoneyFromDrawer = (cash, amount, name) => {
+    // console.log('getMoneyFromDrawer', amount, name);
+
+    const checkCash = cash.map((x) => {
+      if (x[0] === name && x[1] > 0) {
+        return [name, x[1] - amount];
+      } if (x[0] === name && x[1] === 0) {
+        return null;
+      }
+      return x;
+    });
+
+    if (checkCash.includes(null)) {
+      return false;
+    }
+    console.log('checkCash', checkCash);
+    return checkCash;
+  };
 
   // получение всех номиналов
   const amountsArr = Object.keys(amounts)
     .sort((a, b) => a - b);
 
   // @sum - то что надо вернуть - 5 dollars
-  // @cash - то из чего можно вернуть
-  // @amount - номинал
-  const getChange = (sumOut) => {
-    let cash = cashInput.slice();
+  // @internalCash - то из чего можно вернуть
+  const getChange = (sumOut, internalCash) => {
+    // нужен ли этот кэш тут или подавать исходный
+    console.log('internalCash', internalCash);
 
-    // @amount - номинал
-    // @name - название банкноты
-    // задача функции - уменьшить кол-во денег в кеше
-    const getMoneyFromDrawer = (amount, name) => {
-      // console.log('getMoneyFromDrawer', amount, name);
-
-      const checkCash = cash.map((x) => {
-        if (x[0] === name && x[1] > 0) {
-          return [name, x[1] - amount];
-        } if (x[0] === name && x[1] === 0) {
-          return null;
-        }
-        return x;
-      });
-
-      if (checkCash.includes(null)) {
-        return false;
-      }
-      cash = checkCash;
-      console.log('cash', cash);
-      return cash;
-    };
+    let cash = internalCash.slice();
+    console.log('-------------------------------');
 
     // у findAmount собственная sum, потому что она уменьшаются рекурсивно
+    // добавить возвращаемое значение
     const findAmount = (sum) => {
       // получил номинал меньший или равный сумме
       console.log('sum', sum);
@@ -90,40 +95,38 @@ const checkCashRegister = (price, banknote, cashInput) => {
       console.log('amount', amount);
 
       // check for existing money in drawer
-      if (getMoneyFromDrawer(amount, amounts[amount])) {
+
+      const newCash = getMoneyFromDrawer(cash, amount, amounts[amount]);
+      if (newCash) {
         console.log('if getMoneyFromDrawer', amount, amounts[amount]);
 
         putChange(amounts[amount], amount);
         return true;
       }
       // поиск меньшего номинала
-      if (getMoneyFromDrawer() === false) {
-        const lowerAmount = amountsArr.filter((x) => x < amount)
-          .pop();
-        if (lowerAmount.length > 0) {
-          findAmount(lowerAmount);
-        }
-        return false;
+      const lowerAmount = amountsArr.filter((x) => x < amount)
+        .pop();
+      if (lowerAmount.length > 0) {
+        findAmount(lowerAmount);
       }
+      return false;
     };
 
     if (!findAmount(sumOut)) {
       return { status: 'INSUFFICIENT_FUNDS', change: [] };
     }
 
-    // console.log(result);
-
     // compare required change with prepared change
-    // временно отключаю, так как на пустом массиве падает ошибка
-    // const diff = getBalance(changeArr) - sum;
-    // if (diff > 0) {
-    //   getChange(diff);
-    // } else {
-    return result;
-    // }
-  };
+    const diff = sumInput - getBalance(result.change);
+    console.log('diff', diff);
 
-  return getChange(sumInput);
+    if (diff > 0) {
+      getChange(diff);
+    }
+    console.log('-------------------------------');
+    return result;
+  };
+  return getChange(sumInput, cashInput);
 };
 
 console.log(checkCashRegister(19.5, 20, [
@@ -137,4 +140,3 @@ console.log(checkCashRegister(19.5, 20, [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100],
 ]));
-
