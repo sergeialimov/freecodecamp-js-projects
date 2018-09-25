f7d8762c3694ee01b75f5fbc83439310b5b71342
Sergei Alimov <8912652@gmail.com>
2018-09-25 16:37:33 +0300

    move putChange to external function

--- cash-register.js
+++ cash-register.js
@@ -33,12 +33,10 @@ const getCashBalance = (cash) => {
   return 0;
 } 
 
-// задача функции - уменьшить кол-во денег в кеше
+// уменьшение кол-ва денег в кеше
 // @amount - номинал
 // @name - название банкноты
-const getMoneyFromDrawer = (cash, amount, name) => {
-  console.log('getMoneyFromDrawer cash', cash, amount, name);
-  
+const getMoneyFromDrawer = (cash, amount, name) => {                                                    console.log('getMoneyFromDrawer cash', cash, amount, name);
   let isMoneyEnough = false;
   const checkCash = cash.map((x) => {
     if (x[0] === name && x[1] > 0) {
@@ -46,16 +44,32 @@ const getMoneyFromDrawer = (cash, amount, name) => {
       return [name, (x[1] - amount).toFixed(2)];
     }
     return x;
-  });
-  console.log('checkCash', checkCash);
-  
-  
-  if (!isMoneyEnough) {
-    console.log('isMoneyEnough', isMoneyEnough);
+  });                                                                                                    console.log('checkCash', checkCash);
+  if (!isMoneyEnough) {                                                                                  console.log('isMoneyEnough', isMoneyEnough);
     return false;
-  }
-  console.log('newCash', checkCash);
+  }                                                                                                      console.log('newCash', checkCash);
   return checkCash;
+};
+
+// Если номинал существует - прибавляет, если нет - добавляет новый
+// @sum - сумма, которую необходимо добавить к сдаче
+// @amount - номинал банкноты
+const putChange = (amount, sum, changeInput) => {
+  let change = changeInput.slice();
+  let isAmountExists = false;
+  const checkAmount = change.map((x) => {
+    if (x[0] === amount) {
+      isAmountExists = true;
+      return [amount, x[1] + sum];
+    }
+    return x;
+  });
+  if (change.length === 0 || !isAmountExists) {
+    change.push([amount, sum]);
+  } else {
+    change = checkAmount;
+  }
+  return change;                                                                                                    console.log('result.change', result.change);
 };
 
 // @price - сумма к оплате
@@ -63,56 +77,28 @@ const getMoneyFromDrawer = (cash, amount, name) => {
 // @cashInput - money in cash drawer
 const checkCashRegister = (price, banknotes, cashInput) => {
   const sumInput = (banknotes - price).toFixed(2);
-  
-  let result = {};
-  if (getCashBalance(cashInput) - sumInput === 0) {
-    console.log('getCashBalance(cashInput)', getCashBalance(cashInput) - sumInput);
+  let result = { status: ''};
+  if (getCashBalance(cashInput) - sumInput === 0) {                                                        console.log('getCashBalance(cashInput)', getCashBalance(cashInput) - sumInput);
     result = { status: 'CLOSED', change: [] };
-  } else {
-    console.log('getCashBalance(cashInput)', getCashBalance(cashInput) - sumInput);
+  } else {                                                                                                 console.log('getCashBalance(cashInput)', getCashBalance(cashInput) - sumInput);
     result = { status: 'OPEN', change: [] };
-  }
-  console.log('----------------- result', result);
+  }                                                                                                       console.log('----------------- result', result);
   
-  // Если номинал существует - прибавляет, если нет - добавляет новый
-  // @sum - сумма, которую необходимо добавить к сдаче
-  // @amount - номинал банкноты
-  const putChange = (amount, sum) => {
-    let isAmountExists = false;
-    const checkAmount = result.change.map((x) => {
-      if (x[0] === amount) {
-        isAmountExists = true;
-        return [amount, x[1] + sum];
-      }
-      return x;
-    });
-    if (result.change.length === 0 || !isAmountExists) {
-      result.change.push([amount, sum]);
-    } else {
-      result.change = checkAmount;
-    }
-    console.log('result.change', result.change);
-  };
-
-  // @sum - то что надо вернуть - 5 dollars
+  // @sum - то что надо вернуть
   // @internalCash - то из чего можно вернуть
-  const getChange = (sumOut, internalCash) => {                                                       console.log('internalCash', internalCash);
-    let cash = internalCash.slice();                                                                  console.log('-------------------------------');
+  const getChange = (sumOut, internalCash) => {                                                          console.log('internalCash', internalCash);
+    let cash = internalCash.slice();                                                                     console.log('-------------------------------');
     let sum = sumOut;
     // у findAmount собственная sum, потому что она уменьшаются рекурсивно
-    const findAmount = (sum) => {                                                                     console.log('sum', sum);
+    const findAmount = (sum) => {                                                                        console.log('sum', sum);
       // получил номинал меньший или равный сумме
       const amount = amountsArr.filter((x) => x <= sum)
-        .pop();                                    
-                                                 
-      // console.log('amount', amount, typeof(amount), amount.pop());
-      cash = getMoneyFromDrawer(cash, amount, amounts[amount]);
-      if (cash) {  
-        console.log('findAmount cash', cash);
-        putChange(amounts[amount], parseFloat(amount));
+        .pop();                                                                                           console.log('amount', amount);
+      const checkCash = getMoneyFromDrawer(cash, amount, amounts[amount]); // false
+      if (checkCash) {                                                                                    console.log('findAmount cash', cash);
+        result.change = putChange(amounts[amount], parseFloat(amount), result.change);
         return true;
-      } else {
-        console.log('findAmount cash', cash);
+      } else if (!checkCash && amount === 0.01) {                                                         console.log('findAmount cash', cash);
         return false;
       }
       // поиск меньшего номинала
@@ -123,26 +109,27 @@ const checkCashRegister = (price, banknotes, cashInput) => {
       }
       return false;
     };
-
     if (!findAmount(sumOut)) {
       return { status: 'INSUFFICIENT_FUNDS', change: [] };
-    }
+    }                                                                                                       console.log('sumInput', sumInput); console.log('getBalance(result.change)', getBalance(result.change));
     // compare required change with prepared change
-    console.log('sumInput', sumInput);
-    console.log('getBalance(result.change)', getBalance(result.change));
-    
     const diff = (sumInput - getBalance(result.change)).toFixed(2);                                          console.log('diff', diff);
     if (diff > 0) {
       getChange(diff, cash);
-    } 
-    console.log('quitting', result);
-                                                                                                          console.log('-------------------------------');
+    }                                                                                                       console.log('quitting', result);
     return result;
   };
   return getChange(sumInput, cashInput);
 };
 
 
-console.log(checkCashRegister(19.75, 20, [
-  ['QUARTER', 0.25],
-]));
+// console.log(
+// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+ 
+ // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
+
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+
+// should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
